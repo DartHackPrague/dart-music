bd05c5f297f0c3c67edc540324fe95fa1ab4dc64
David Votrubec <votrubec.d@st-software.com>
2012-04-28 11:28:17 +0200

    DartMusic added ... test

---
+++ DartMusic/DartMusic.dart
@@ -0,0 +1,20 @@
+#import('dart:html');
+
+class DartMusic {
+
+  DartMusic() {
+  }
+
+  void run() {
+    write("Hello World!");
+  }
+
+  void write(String message) {
+    // the HTML library defines a global "document" variable
+    document.query('#status').innerHTML = message;
+  }
+}
+
+void main() {
+  new DartMusic().run();
+}
