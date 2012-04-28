ff0aa465f410a51d100f6741664da3bb2d678b0b
Tibor Szolár <tibor.szolar@seznam.cz>
2012-04-28 14:13:36 +0200

    Added loading audio source from <audio> tag.

--- DartMusic/DartMusic.dart
+++ DartMusic/DartMusic.dart
@@ -1,4 +1,5 @@
 #import('dart:html');
+#import('dart:dom', prefix:'dom');
 
 class DartMusic {
 
@@ -13,8 +14,30 @@ class DartMusic {
     // the HTML library defines a global "document" variable
     document.query('#status').innerHTML = message;
   }
+  
+  void registerAudio() {
+    //getting source from audio tag
+    dom.AudioContext audioContext = new dom.AudioContext();
+    var audio = document.query("audio");
+    var source = audioContext.createMediaElementSource(audio);
+    dom.AudioGainNode gainNode = audioContext.createGainNode();
+
+    //connecting inputs and outputs
+    source.connect(gainNode, 0, 0);
+    gainNode.connect(audioContext.destination, 0, 0);
+
+    //volume slider handler
+    document.query("#volume").on.change.add((e) {
+      var volume = Math.parseInt(e.target.value);
+      var max = Math.parseInt(e.target.max);
+      var fraction = volume / max;
+      gainNode.gain.value = fraction * fraction;
+    });
+  }
 }
 
 void main() {
-  new DartMusic().run();
+  DartMusic m = new DartMusic();
+  m.run();
+  m.registerAudio();
 }
