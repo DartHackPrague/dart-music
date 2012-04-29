8a4b34ab7a7675d6270ea3635c13bd415185f8b2
Tibor Szolár <tibor.szolar@seznam.cz>
2012-04-29 09:10:34 +0200

    Cleared some legacy code (registerDragNDrop() method).

--- DartMusic/DartMusic.dart
+++ DartMusic/DartMusic.dart
@@ -42,37 +42,6 @@ class DartMusic {
     window.setInterval(f() => this.update(), this._delay);
   }
 
-
-  void registerDragNDrop() {
-    document.on.drop.add( function( MouseEvent event ) {
-      event.preventDefault();
-      event.stopPropagation();
-      //obtaining file path
-      FileList files = event.dataTransfer.files;
-      FileReader reader = new FileReader();
-      reader.on.loadStart.add((Event e) { print("load start"); });
-      reader.on.progress.add((Event e) { print("progress"); });
-      reader.on.error.add((Event e) { print("error"); });
-
-      reader.on.load.add( (Event e) {
-        print("file loaded ");
-        AudioElement audioOld = document.query("audio");
-
-        AudioElement audio = new AudioElement();
-        audio.src = e.target.result;
-        audio.controls = true;
-        audioOld.replaceWith(audio);
-        //document.body.nodes.add(audio);
-      });
-      for(int i = 0; i < files.length; i++) {
-        File file = files.item(i);
-        print("dragged file: "+file.name);
-        reader.readAsDataURL(file);
-      }
-
-    });
-  }
-
   void addEffect(IRenderer effect) {
     this._effects.add(effect);
   }
