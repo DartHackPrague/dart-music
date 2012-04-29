83db06e8b914532f448cffcb01116d557e924d62
David Votrubec <votrubec.d@st-software.com>
2012-04-29 16:52:46 +0200

    Added easterEgg - dart logo in canvas

--- DartMusic/EasterEgg.dart
+++ DartMusic/EasterEgg.dart
@@ -1,7 +1,40 @@
 class EasterEgg {
 
+  CanvasElement _canvas = null;
+  CanvasRenderingContext2D _ctx;
+  int top = 100;
+  int left = 100;
+
+  String pathToImage = "images/dart_logo.png";
+
   void Surprise() {
-    window.alert('WAT ??');
+    if (_canvas == null) {
+      _canvas = document.query("#easterEgg");
+      _ctx = this._canvas.getContext("2d");
+
+      _canvas.style.position = "absolute";
+      _canvas.style.top = top.toString() + 'px';
+      _canvas.style.left = left.toString() + 'px';
+    }
+
+    _LoadImage();
+
+    _DoCrazyMovements();
+  }
+
+  void _LoadImage() {
+    var myImage = new Element.tag("img");
+    myImage.on.load.add((_) {
+      _ctx.drawImage(myImage, 0, 0);
+    });
+
+    myImage.src = pathToImage;
+  }
+
+  void _DoCrazyMovements() {
+    _canvas.style.position = "absolute";
+    _canvas.style.top = '400px';
+    _canvas.style.left = '600px';
   }
 
 }
