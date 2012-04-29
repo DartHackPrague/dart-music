3c762c7e39eee174f36e10edaa46b02b75187f5c
Tibor Szolár <tibor.szolar@seznam.cz>
2012-04-29 10:43:08 +0200

    Debugging and refactoring color rotations.

--- DartMusic/BgColorAnimator.dart
+++ DartMusic/BgColorAnimator.dart
@@ -8,6 +8,9 @@ class BgColorAnimator {
   int minColorValue = 0;
   bool minReached = false;
   bool maxReached = false;
+  bool incrementing = true; 
+  final int colorChangeStep = 10;
+  final int colorChangeDelay = 200;
 
   BgColorAnimator(Element element) {
     this.element = element;
@@ -28,40 +31,71 @@ class BgColorAnimator {
     return randomColor;
   }
 
-
   RgbColor getNextColor(RgbColor currentColor) {
-    if (maxReached == false && (currentColor.r <= maxColorValue || currentColor.g <= maxColorValue || currentColor.b <= maxColorValue)) {
-      if (currentColor.r < maxColorValue) {
-        currentColor.r++;
+    if(incrementing) {
+      if(!isMaximal(currentColor.r)) {
+        currentColor.r += colorChangeStep;
+      } else if(!isMaximal(currentColor.g)) {
+        currentColor.g += colorChangeStep;
+      } else if(!isMaximal(currentColor.b)) {
+        currentColor.b += colorChangeStep;
+      } else {
+        print("reached max");
+        incrementing = false;
       }
-      else if (currentColor.g < maxColorValue) {
-        currentColor.g++;
-      }
-      else if (currentColor.b < maxColorValue) {
-        currentColor.b++;
-      }
-      else {
-        maxReached = true;
-        minReached = false;
-      }
-    }
-    else if (maxReached == true) {
-      if (currentColor.r > minColorValue && currentColor.r <= maxColorValue) {
-        currentColor.r--;
-      }
-      else if (currentColor.g > minColorValue && currentColor.g <= maxColorValue) {
-        currentColor.g--;
-      }
-      else if (currentColor.b > minColorValue && currentColor.b <= maxColorValue) {
-        currentColor.b--;
-      }
-      else {
-        maxReached = false;
-        minReached = true;
+    } else {
+      if(!isMinimal(currentColor.r)) {
+        currentColor.r += colorChangeStep;
+      } else if(!isMinimal(currentColor.g)) {
+        currentColor.g += colorChangeStep;
+      } else if(!isMinimal(currentColor.b)) {
+        currentColor.b += colorChangeStep;
+      } else {
+        print("reached min");
+        incrementing = true;
       }
     }
-
+//    
+//    if (maxReached == false && (currentColor.r <= maxColorValue || currentColor.g <= maxColorValue || currentColor.b <= maxColorValue)) {
+//      if (currentColor.r < maxColorValue) {
+//        currentColor.r++;
+//      }
+//      else if (currentColor.g < maxColorValue - colorChangeStep) {
+//        currentColor.g++;
+//      }
+//      else if (currentColor.b < maxColorValue - color) {
+//        currentColor.b++;
+//      }
+//      else {
+//        maxReached = true;
+//        minReached = false;
+//      }
+//    }
+//    else if (maxReached == true) {
+//      if (currentColor.r > minColorValue && currentColor.r <= maxColorValue) {
+//        currentColor.r--;
+//      }
+//      else if (currentColor.g > minColorValue && currentColor.g <= maxColorValue) {
+//        currentColor.g--;
+//      }
+//      else if (currentColor.b > minColorValue && currentColor.b <= maxColorValue) {
+//        currentColor.b--;
+//      }
+//      else {
+//        maxReached = false;
+//        minReached = true;
+//      }
+//    }
+//
     return currentColor;
+  }
+  
+  bool isMaximal(int colorValue) {
+    return (colorValue + colorChangeStep) > maxColorValue;
+  }
+  
+  bool isMinimal(int colorValue) {
+    return (colorValue - colorChangeStep) < minColorValue;
   }
 
   void perpetualColorChange() {
@@ -70,7 +104,7 @@ class BgColorAnimator {
 
     changeBgColor();
 
-    window.setTimeout(perpetualColorChange, 15);
+    window.setTimeout(perpetualColorChange, colorChangeDelay);
 
   }
 }
